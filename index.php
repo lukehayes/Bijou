7ea58084c589eacf43fb06336ea215dda8add7b3
lukehayes <lukepurplehayes@gmail.com>
2021-07-19 09:53:55 +0100

    Index.php holds simple test routing.

--- index.php
+++ index.php
@@ -2,5 +2,22 @@
 require "vendor/autoload.php";
 
 use MVP\App;
+use MVP\View;
 
-var_dump(App::create());
+$app = new App;
+
+$app->getRouter()->get("/", function($request)
+{
+    echo $request->path;
+});
+
+$app->getRouter()->get("/hello", function($request)
+{
+    $view = new View();
+    $view->render('hello');
+});
+
+$app->getRouter()->get("/other", function($request)
+{
+    echo $request->path;
+});
