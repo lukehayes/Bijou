7e186ca78a4b2ccbfc02b47e3b4f8415bbc6031f
lukehayes <lukepurplehayes@gmail.com>
2021-07-19 09:17:29 +0100

    Added MVP\View::set() method.

--- src/View.php
+++ src/View.php
@@ -29,4 +29,18 @@ class View
     {
         require_once $this->templatePath . $template . ".php";
     }
+
+    /**
+     * Set a variable so that it is available inside the view.
+     *
+     * @param string $name    The name of the data.
+     * @param mixed  $value   The value of the data.
+     *
+     * @return void
+     */
+    public function set(string $name, $value) : void
+    {
+        $this->$name = $value;
+    }
+
 }
