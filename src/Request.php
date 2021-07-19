52646876b93cde34440b7f528723420150535e3a
lukehayes <lukepurplehayes@gmail.com>
2021-07-19 13:59:56 +0100

    Added MVP\Request::isPost() and MVP\Request::isGet() methods.

--- src/Request.php
+++ src/Request.php
@@ -13,6 +13,26 @@ class Request
         $this->path = $_SERVER['REQUEST_URI'];
         $this->method = $_SERVER['REQUEST_METHOD'];
     }
+
+    /**
+     * Is the current request GET or not?
+     *
+     * @return bool
+     */
+    public function isGet() : bool
+    {
+        return $this->method == "GET" ?? false;
+    }
+
+    /**
+     * Is the current request POST or not?
+     *
+     * @return bool
+     */
+    public function isPost() : bool
+    {
+        return $this->method == "POST" ?? false;
+    }
 }
 
 
