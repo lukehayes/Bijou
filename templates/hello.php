dbb35dd8e3ae669da0d746fab93a5f0b3039c171
lukehayes <lukepurplehayes@gmail.com>
2021-07-19 09:41:39 +0100

    Added templates/hello.php to project.

---
+++ templates/hello.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Hello.</title>
+</head>
+<body>
+
+<?php echo $this->test; ?>
+
+<p>Hello!</p>
+    
+</body>
+</html>
