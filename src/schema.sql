5af0ab1f47573ab6dfc890a64cd49348fed7f6db
lukehayes <lukepurplehayes@gmail.com>
2021-09-16 09:21:07 +0100

    Added extra row to schema.sql.

--- src/schema.sql
+++ src/schema.sql
@@ -4,5 +4,6 @@ DROP TABLE if exists users;
 CREATE TABLE users(id integer PRIMARY KEY, username text, email integer, password text);
 
 INSERT INTO users(id, username, email, password) VALUES
-    (1, "developer", "developer@app.com", "$2y$10$9cLVIkAV5U4zKz/wI4MGEexSk91/e5TS6DODu/Dkd/Zkih19OpA7O");
+    (1, "developer", "developer@app.com", "$2y$10$9cLVIkAV5U4zKz/wI4MGEexSk91/e5TS6DODu/Dkd/Zkih19OpA7O"),
+    (2, "admin", "admin@app.com", "$2y$10$9cLVIkAV5U4zKz/wI4MGEexSk91/e5TS6DODu/Dkd/Zkih19OpA7O");
 
