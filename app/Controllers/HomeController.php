5b00900ed26c8245067608d485a60bfc7d475e0b
Luke Hayes <lukepurplehayes@gmail.com>
2021-12-08 17:19:38 +0000

    Added app/Controllers directory to project.

---
+++ app/Controllers/HomeController.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\Controllers;
+
+use Bijou\Controller;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        dump("Class Loaded");
+    }
+}
